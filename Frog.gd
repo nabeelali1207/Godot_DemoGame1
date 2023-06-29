472fa8d2944c4808e11c443517b395366e054f63
Nabeel Ali <nabeel1207ali@gmail.com>
2023-06-29 17:22:29 +0500

    added frog death sequence

--- Frog.gd
+++ Frog.gd
@@ -9,6 +9,7 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 @onready var animation = get_node("AnimationPlayer")
 var player
 var chase = false
+var alive = true
 
 
 
@@ -45,9 +46,9 @@ func _physics_process(delta):
 		animation.play("Jump")
 	elif velocity.y >= 10:
 		animation.play("Fall")
-	else:
+	elif alive == true:
 		animation.play("Idle")
-
+		
 	move_and_slide()
 
 
@@ -62,3 +63,13 @@ func _on_player_detection_body_exited(body):
 	if body.name == "Player":
 		chase = false
 		velocity.x = 0
+
+
+func _on_player_death_body_entered(body):
+	if body.name == "Player":
+		chase = false
+		alive = false
+		get_node("AnimatedSprite2D").play("Death")
+		await get_node("AnimatedSprite2D").animation_finished
+		self.queue_free()
+		
